db7d6553e6ee44aed6550231f5faeda45baa88f2
unlocker-sy <soonyoung87@gmail.com>
2018-04-29 23:01:04 +0900

    코드 정리

--- GettingHtml.py
+++ GettingHtml.py
@@ -1,29 +1,27 @@
 #-*- coding: utf-8 -*-
-#from urllib2.request import urlopen
-#from urllib2.request import HTTPError
 from urllib2 import urlopen
 from urllib2 import HTTPError
 from bs4 import BeautifulSoup
 import sys
 
 print('-----------')
-# urllib.request 패키지의 urlopen()함수를 import.
-# 웹페이지(url)에서 html객체를 초기화한다. 
 
-# html.read()함수로 html내용을 읽어들인다.
-# html.parser는 파이썬에 기본적으로 내장되어있는 파서라고 한다. html.parser대신에 lxml을 쓸 수도 있다고 한다.
 try:
-	#html = urlopen('http://www.pythonscraping.com/pages/page1.html')
     html = urlopen("https://www.modetour.com/main/")
 except HTTPError as e:
 	print(e)
 else:
+	# html.read()함수로 html내용을 읽어들임
 	bsObj = BeautifulSoup(html.read(), "html.parser")
+	# html.parser는 파이썬에 기본적으로 내장되어있는 파서, html.parser대신에 lxml을 쓸 수도 있음
+
+
 	#print('==== full tag ====')
 	#print(bsObj)
 	#print('==== html ====')
 	#print(bsObj.html)
 	#print(bsObj.head)
+	
 	print(bsObj.head.title)
 	print('==== h1 ====')
 	print(bsObj.h1)
@@ -33,15 +31,5 @@ else:
 	print(bsObj.body.h2)
 	print(bsObj.find_all('h2'))
 
+	print('type (body.h2): ', type(bsObj.body.h2))
 
-'''
-print('========')
-#print(bsObj)
-print('========')
-#print(bsObj.html)
-print('========')
-#print(bsObj.h1)
-#print(bsObj.html.body.h1)
-#print(bsObj.html.h1)
-#print('========')
-'''
